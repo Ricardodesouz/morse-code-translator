f86188b3a988b23d0e0f6f0d8f37644017458381
Ricardodesouz <ricardodesouza360@gmail.com>
2024-04-11 21:49:10 -0300

    organizacao do codigo

---
+++ src/Application/Main.java
@@ -0,0 +1,13 @@
+package Application;
+
+import model.services.MorseTranslator;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MorseTranslator morseTranslator = new MorseTranslator();
+
+        morseTranslator.forMorse();
+        }
+
+    }
